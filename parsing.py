1571928759539ba0b342d83cb6ef1c9c33a2d71c
gbrs <gbrs@mail.ru>
2021-05-03 11:34:12 +0300

    спарсил криво поисковую выдачу Яндекса

--- parsing.py
+++ parsing.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://kvadromash.ru'
+url = 'https://yandex.ru/search/?clid=9582&text=металлообработка%20в%20спб&l10n=en-US&lr=2'
 response = requests.get(url)
 # print(response.text)
 
 soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('p')
+quotes = soup.find_all('div')
 
 for quote in quotes:
     print(quote.text)
