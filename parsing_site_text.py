fb396eee01b4e1e27db759bd8f32d648ef074b75
gbrs <61385511+gbrs@users.noreply.github.com>
2021-05-14 09:34:39 +0300

    чтение абзацев пополнил чтением списков (маркированных и нумерованных)

--- parsing_site_text.py
+++ parsing_site_text.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://steelroof.ru/gibka/'
+url = 'https://metalloobrabotkaspb.ru/'
 response = requests.get(url)
 # print(response.text)
 
 soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('p')
+quotes = soup.find_all('p') + soup.find_all('ul') + soup.find_all('ol')
+# абзацы, списки (маркированные и нумерованные)
 
 for quote in quotes:
     print(quote.text)
