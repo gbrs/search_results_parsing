8b6b8f0035a8b346b73321d4577e2e3789c52345
gbrs <gbrs@mail.ru>
2021-05-15 11:28:41 +0300

    выделил предыдущее в функцию. Считывая из файла адреса, добавляю их текст к файлу

--- parsing_site_text.py
+++ parsing_site_text.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-def parse_site(site_name):
+def parse_site(url):
+    '''
+    парсит текст с сайта по указанному адресу
+    :param url: адрес сайта, который парсим
+    :return: None. (До)записывает в файл текст сайта
+    '''
 
-    url = 'https://ferropolis.ru/'
     response = requests.get(url)
 
     #  исправляем кодировку на некоторых сайтах
@@ -16,8 +20,17 @@ def parse_site(site_name):
     with open('output.txt', 'a', encoding='utf-8') as f:
         for quote in quotes:
             txt = quote.text
-            f.write(txt)
-            f.write('\n')
+            if txt != '\n':  # TODO не работает условие - куча пустых строк. Почему?
+                f.write(txt)
+                f.write('\n')
             # quote.txt здесь возвращал None, а внутри print - строку.
             # пришлось ввести промежуточную переменную txt
+        f.write(3 * '_____________________________________________________________________________\n')
 
+
+with open('urls.txt', encoding='utf-8') as f:
+    urls = f.read().split()
+print(urls)
+
+for url in urls:
+    parse_site(url)
